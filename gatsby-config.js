52823225ad3589131cbf80bb44ee4d027bf482a9
Zach Blocker <zach.blocker@gmail.com>
2022-11-06 18:23:40 -0500

    Added RSS feed (no HTML content)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const siteConfig = require('./config.js')
 module.exports = {
   siteMetadata: {
     title: siteConfig.title,
+    description: "Zach Blocker's professional portfolio and blog",
     siteUrl: siteConfig.url,
   },
   plugins: [
@@ -50,5 +51,59 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-plugin-feed-mdx',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: site.siteMetadata.siteUrl + "/blog/" + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + "/blog/" + edge.node.fields.slug,
+                });
+              });
+            },
+            query: `
+              {
+                allMdx(
+                  filter: {fields: {contentKey: {eq: "blog"}}}
+                  sort: {fields: fields___sequence, order: DESC}
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      fields {
+                        date
+                        slug
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Zach on Code Blog",
+          }
+        ]
+      }
+    }
   ]
 };
